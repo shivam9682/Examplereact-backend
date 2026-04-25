a1f482dded81106c201c38f7c70014ca02bb30c7
shivam9682 <ssivamyadav0123@gmail.com>
2026-04-25 16:16:54 +0530

    Update AuthController.java

--- src/main/java/in/sp/spring/controller/AuthController.java
+++ src/main/java/in/sp/spring/controller/AuthController.java
@@ -25,12 +25,10 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/auth")
 //@CrossOrigin(origins = "http://localhost:5173")
-<<<<<<< HEAD
+
 
     @CrossOrigin(origins = "*")
-=======
-@CrossOrigin(origins = "*")
->>>>>>> 1331dab (message)
+
 public class AuthController {
 
     @Autowired

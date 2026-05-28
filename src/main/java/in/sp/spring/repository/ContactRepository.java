4418aaa04c5b96fafa9caf6a64ae14c9353124f6
shivam9682 <ssivamyadav0123@gmail.com>
2026-05-28 16:47:30 +0530

    Update ContactRepository.java

--- src/main/java/in/sp/spring/repository/ContactRepository.java
+++ src/main/java/in/sp/spring/repository/ContactRepository.java
@@ -1,9 +1,11 @@
 package in.sp.spring.repository;
 
-import com.example.contactbackend.entity.ContactMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import in.sp.spring.Entity.ContactMessage;
+
 @Repository
 public interface ContactRepository extends JpaRepository<ContactMessage, Long> {
+
 }

c8d1e1f8bb432ab4777fdb50f6440e1c4f234c61
shivam9682 <ssivamyadav0123@gmail.com>
2026-05-28 16:58:26 +0530

    Update ContactMessage.java

--- src/main/java/in/sp/spring/Entity/ContactMessage.java
+++ src/main/java/in/sp/spring/Entity/ContactMessage.java
@@ -1,27 +1,64 @@
 package in.sp.spring.Entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+
 
 @Entity
 @Table(name = "contact_messages")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
 public class ContactMessage {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+     @Column
     private String name;
-
+    @Column
     private String email;
-
+    @Column
     private String subject;
 
     @Column(columnDefinition = "TEXT")
     private String message;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+    
 }

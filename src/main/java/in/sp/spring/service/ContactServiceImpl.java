46101adfbe293eba685ec85a90833e813b4b4d0a
shivam9682 <ssivamyadav0123@gmail.com>
2026-05-28 17:13:10 +0530

    Update ContactServiceImpl.java

--- src/main/java/in/sp/spring/service/ContactServiceImpl.java
+++ src/main/java/in/sp/spring/service/ContactServiceImpl.java
@@ -1,5 +1,20 @@
 package in.sp.spring.service;
 
-public class ContactServiceImpl {
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import in.sp.spring.Entity.ContactMessage;
+import in.sp.spring.repository.ContactRepository;
+
+@Service
+public class ContactServiceImpl implements ContactService {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Override
+    public ContactMessage saveMessage(ContactMessage contactMessage) {
+        return contactRepository.save(contactMessage);
+    }
 }

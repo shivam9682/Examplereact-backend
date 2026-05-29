590036ab80b93133df28ee04d0fdcc5b2a45b883
shivam9682 <ssivamyadav0123@gmail.com>
2026-05-29 15:41:56 +0530

    Update ContactRepository.java

--- src/main/java/in/sp/spring/repository/ContactRepository.java
+++ src/main/java/in/sp/spring/repository/ContactRepository.java
@@ -1,11 +1,6 @@
 package in.sp.spring.repository;
 
-<<<<<<< HEAD
-=======
 
-
-
->>>>>>> 098ecfa (Fixed List imports)
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,10 +8,9 @@ import in.sp.spring.Entity.ContactMessage;
 
 @Repository
 public interface ContactRepository extends JpaRepository<ContactMessage, Long> {
-<<<<<<< HEAD
 
-}
-=======
+
+
 	 long countByIsReadFalse();
 }
->>>>>>> 098ecfa (Fixed List imports)
+

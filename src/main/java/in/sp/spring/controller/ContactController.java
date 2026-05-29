ae6de6ca1cb653d8f1140abf5f53f9eeb17cd24f
shivam9682 <ssivamyadav0123@gmail.com>
2026-05-29 14:29:03 +0530

    Update ContactController.java

--- src/main/java/in/sp/spring/controller/ContactController.java
+++ src/main/java/in/sp/spring/controller/ContactController.java
@@ -3,7 +3,7 @@ package in.sp.spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.List;
 import in.sp.spring.Entity.ContactMessage;
 import in.sp.spring.repository.ContactRepository;
 import in.sp.spring.service.ContactService;

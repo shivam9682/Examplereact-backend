63de52a115c1882fead74a780fde5596fc19c633
shivam9682 <ssivamyadav0123@gmail.com>
2026-05-30 14:04:43 +0530

    Fix formatting of setIsRead method call

--- src/main/java/in/sp/spring/controller/ContactController.java
+++ src/main/java/in/sp/spring/controller/ContactController.java
@@ -66,7 +66,7 @@ public class ContactController {
                 contactRepository.findAll();
 
         for (ContactMessage msg : messages) {
-          msg.setIsRead(true); 
+          msg.setIsRead(true);
         }
 
         contactRepository.saveAll(messages);
